3c60c6c70ca5c15e7180307d8406d5aaee68c10c
Narine <narineshankyan@yandex.ru>
2024-05-09 01:38:17 +0300

    add grid component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,3 @@
-import './App.css';
-import {Header} from './components/Header/Header'
-import {Main} from './components/Main/Main'
-import {Footer} from './components/Footer/Footer'
-import { Card } from './components/Card/Card';
-import { Grid } from './components/Grid/Grid';
 
 
 function App() {
