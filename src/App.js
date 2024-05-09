9d1868f4cc46fab65ed6dd4cb4fd0447069f51fc
Narine <narineshankyan@yandex.ru>
2024-05-09 21:54:44 +0300

    Move Grid to Main

--- src/App.js
+++ src/App.js
@@ -3,15 +3,12 @@ import { Header } from "./components/Header/Header";
 import { Main } from "./components/Main/Main";
 import { Footer } from "./components/Footer/Footer";
 import NewProducts from "./components/NewProducts/NewProducts";
-import { Grid } from './components/Grid/Grid';
-
 function App() {
   return (
     <>
       <Header />
-      {/* <Main /> */}
-      <Footer />
-      <Grid/>    
+      <Main />
+      <Footer />     
       <NewProducts />
     </>
   );
