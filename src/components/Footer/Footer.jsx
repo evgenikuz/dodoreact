7e996913e1e12c158257b969adf5a9cad3fc1ebe
Nahid Abduldjalilov <nahid_323@mail.ru>
2024-05-10 13:22:32 +0300

    [FIX] styles + add Container

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,37 +7,39 @@ import instagram from "./img/instagramm.png";
 import odnoklassniki from "./img/odnoklassniki.png";
 import vk from "./img/vk.png";
 import youtube from "./img/youtube.png";
+import { Container } from "../Container/Container";
 
 export const Footer = () => {
   let year = new Date().getFullYear();
   return (
     <footer>
       <div className={s.footer}>
+      <Container>
         <div className={s.footerup}>
           <div className={s.footerleft}>
             <div className={s.footerleft__up}>
-              <ul>
+              <ul className={s.footer__ul}>
                 Додо пицца
-                <li>О нас</li>
-                <li>Блог «Сила ума»</li>
-                <li>Додо ИС</li>
+                <li className={s.footer__li}>О нас</li>
+                <li className={s.footer__li}>Блог «Сила ума»</li>
+                <li className={s.footer__li}>Додо ИС</li>
               </ul>
-              <ul>
+              <ul className={s.footer__ul}>
                 Работа
-                <li>В пиццерии</li>
-                <li>В контакт- центре</li>
+                <li className={s.footer__li}>В пиццерии</li>
+                <li className={s.footer__li}>В контакт- центре</li>
               </ul>
-              <ul>
+              <ul className={s.footer__ul}>
                 Партнерам
-                <li>Франшиза</li>
-                <li>Инвестиции</li>
-                <li>Поставщикам</li>
-                <li>Предложить помещение</li>
+                <li className={s.footer__li}>Франшиза</li>
+                <li className={s.footer__li}>Инвестиции</li>
+                <li className={s.footer__li}>Поставщикам</li>
+                <li className={s.footer__li}>Предложить помещение</li>
               </ul>
-              <ul>
+              <ul className={s.footer__ul}>
                 Это интересно
-                <li>Экскурсии и мастер-классы</li>
-                <li>Корпоративные заказы</li>
+                <li className={s.footer__li}>Экскурсии и мастер-классы</li>
+                <li className={s.footer__li}>Корпоративные заказы</li>
               </ul>
             </div>
             <div className={s.footerleft__down}>
@@ -72,7 +74,7 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-        <hr />
+        <hr className={s.footer__hr} />
         <div className={s.footerdown}>
           <div className={s.footerdown__left}>
             <img src={logo} alt="logo" />
@@ -80,10 +82,10 @@ export const Footer = () => {
           </div>
 
           <div className={s.footerdown__center}>
-            <ul>
-              <li>Правовая информация</li>
-              <li>Калорийность и состав</li>
-              <li>Помощь</li>
+            <ul className={s.footer__ul}>
+              <li className={s.footer__li}>Правовая информация</li>
+              <li className={s.footer__li}>Калорийность и состав</li>
+              <li className={s.footer__li}>Помощь</li>
             </ul>
           </div>
           <div className={s.footerdown__right}>
@@ -110,6 +112,7 @@ export const Footer = () => {
             </a>
           </div>
         </div>
+      </Container>
       </div>
     </footer>
   );
